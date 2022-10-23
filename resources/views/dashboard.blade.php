619b6c46363ced2a99da1a45b10ca9dcf80b5fc2
parvazreza00 <parvazreza00@gmail.com>
2022-10-23 10:33:50 +0600

    dashboard update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Hi....') }} {{ Auth::user()->name }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
-            </div>
+           This is the dashboard page
         </div>
     </div>
 </x-app-layout>
